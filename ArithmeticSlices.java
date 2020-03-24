a2034bca91ad676a0044bfa29de22a56a2b3a730
Vaishnavi Ramesh <vramesh@pulsesecure.net>
2020-03-23 23:40:01 -0700

    DP-8

---
+++ ArithmeticSlices.java
@@ -0,0 +1,21 @@
+//TC : O(n) where n is the length of A
+//SC : O(n) where n is the length of A
+class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        if(A.length == 0 || A == null)
+            return 0;
+        
+        int[] dp = new int[A.length];
+        
+        int count = 0;
+        
+        for(int i=dp.length-3;i>=0;i--){
+            if(A[i+1] - A[i] == A[i+2] - A[i+1] ){
+                dp[i] = 1 + dp[i+1];
+                count = count + dp[i];
+            }
+        }
+        
+        return count;
+    }
+}
