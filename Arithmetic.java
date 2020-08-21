f67f319d225f3792d1c92e6dfb8c1d1a411c694d
PadmaPriyaCheruvu <65566756+PadmaPriyaCheruvu@users.noreply.github.com>
2020-08-21 10:30:38 -0500

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -17,3 +17,23 @@ class Solution {
     }
 }
 
+//Time-O(n)
+//Space-O(1)
+class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        if(A==null||A.length==0)
+            return 0;
+        int n = A.length;
+        int sum = 0;
+        int prev = 0;
+        for(int i=2;i<n;i++){
+            if(A[i]-A[i-1] == A[i-1]-A[i-2]){
+                prev+=1;
+                sum+=prev;
+            }else{
+                prev=0;
+            }
+        }
+        return sum;
+    }
+}
