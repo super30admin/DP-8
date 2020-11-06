96f922dcb9f8df1b3fc7f1909c723673005016ee
laxmikantbpandhare <laxmikantpandhare@gmail.com>
2020-11-06 13:46:28 -0800

    added file

--- 120_Triangle.java
+++ 120_Triangle.java
@@ -1,3 +1,21 @@
+    /*  Explanation
+    # Leetcode problem link : https://leetcode.com/problems/triangle/
+    Time Complexity for operators : O(1/2 * (n-1) * n) … n = is the size of List
+    Extra Space Complexity for operators : O(n) .. n = is the size of List
+    Did this code successfully run on Leetcode : NA
+    Any problem you faced while coding this : No
+# Your code here along with comments explaining your approach
+        # Basic approach : 
+        # Optimized approach: 
+                              
+            # 1. 
+                    A) Create an dp array of size n, n is the size of triangle.
+                    B) Fill all the values of the last row from the triangle.
+                    C) Now two for loops for the iteration, and get the
+                        dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
+                    D) At the end, our answer will be at first index of array return it.
+    */ 
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         if(triangle == null || triangle.size() == 0)
--- 413_Arithmatic_Slices.java
+++ 413_Arithmatic_Slices.java
@@ -1,3 +1,22 @@
+    /*  Explanation
+    # Leetcode problem link : https://leetcode.com/problems/arithmetic-slices/
+    Time Complexity for operators : o(n) .. n is the length of the array
+    Extra Space Complexity for operators : o(1) 
+    Did this code successfully run on Leetcode : NA
+    Any problem you faced while coding this : No
+# Your code here along with comments explaining your approach
+        # Basic approach : 
+        # Optimized approach: 
+                              
+            # 1. 
+                    A) Start from the end by A.length-2 position.
+                    B) Check if different between 3 elements is same if it is then 
+                        prev = 1 + prev
+                        sum += prev
+                    C) If not then, prev = 0.
+                    D) At the end, return sum.
+    */ 
+
 class Solution {
     public int numberOfArithmeticSlices(int[] A) {
         if(A == null || A.length == 0)
