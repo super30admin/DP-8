6700375919cb128a8a37ca9a3046d28bbfbb03d3
Rishi Raj <rishiraj824@gmail.com>
2021-02-05 20:56:33 -0500

    Update arithmetic-slices.java

--- arithmetic-slices.java
+++ arithmetic-slices.java
@@ -1,3 +1,25 @@
+// Time - O(N), Space - O(1)
+
+class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        if(A == null || A.length < 3) {
+            return 0;
+        }
+    
+        int[] dp = new int[A.length];    
+        int diff = A[1] - A[0];
+        int count = 0;
+        for(int i=2;i<A.length; i++) {            
+            if(A[i] - A[i-1] == A[i-1] - A[i-2]) {
+                dp[i] = 1 + dp[i-1];
+                count += dp[i];
+            }
+        }
+        
+        return count;
+    }
+}
+
 // Time - O(N^2), Space -O(1)
 
 class Solution {
