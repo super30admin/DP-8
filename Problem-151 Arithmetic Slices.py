e3796ca832284624c6a277dd579347612e6f52df
Adhish Shrivastava <adhish70@gmail.com>
2022-01-04 09:01:20 -0500

    Problem 151 done

--- Problem-151 Arithmetic Slices.py
+++ Problem-151 Arithmetic Slices.py
@@ -1,11 +1,27 @@
 # 413. Arithmetic Slices
 # https://leetcode.com/problems/arithmetic-slices/
 
-# Logic: 
+# Logic: We can have a dp variable that will have the count of 
+# arithmetic slice at that point. We also have a running sum 
+# variable that will give us the count of all the possible 
+# arithmatic slices.
 
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        pass
+        if len(nums) < 3:
+            return 0
+        
+        dp = 0
+        count = 0
+        
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                dp = dp + 1
+                count += dp
+            else:
+                dp = 0
+        
+        return count
