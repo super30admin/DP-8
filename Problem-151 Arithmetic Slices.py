ce5412efdf24f0d3c1f2969414e5c33b8c7df6f8
Adhish Shrivastava <adhish70@gmail.com>
2022-01-04 08:59:16 -0500

    Initial Commit

---
+++ Problem-151 Arithmetic Slices.py
@@ -0,0 +1,11 @@
+# 413. Arithmetic Slices
+# https://leetcode.com/problems/arithmetic-slices/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        pass
---
+++ Problem-152 Triangle.py
@@ -0,0 +1,11 @@
+# 120. Triangle
+# https://leetcode.com/problems/triangle/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        pass
