1eff3098cd96a30f4ee237c1aad1ca06d5d23bed
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-06-19 22:51:14 -0700

    Add files via upload

---
+++ Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array
+{
+    public class Triangle
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            int sum = 0;
+
+            int n = triangle.Count;
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    triangle[i][j] = triangle[i][j] + Math.Min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+                }
+            }
+
+            return triangle[0][0];
+        }
+    }
+}
