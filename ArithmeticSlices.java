b3c27bc118d33c3246e85ea60f44bb45f8ba714b
Hanuman Reddy <39452135+HanumanReddy1212@users.noreply.github.com>
2022-08-11 22:23:19 -0700

    dp-8 done

---
+++ ArithmeticSlices.java
@@ -0,0 +1,20 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+public class ArithmeticSlices {
+    public int numberOfArithmeticSlices(int[] nums) {
+        // null
+        if(nums == null || nums.length == 0) return 0;
+        int count = 0;
+        int n = nums.length;
+        int prev = 0;
+        for(int i = 2; i < n; i++) {
+            int curr = 0;
+            if(nums[i-1] - nums[i-2] == nums[i] - nums[i-1]) {
+                curr = prev + 1;
+                count += curr;
+            }
+            prev = curr;
+        }
+        return count;
+    }
+}
