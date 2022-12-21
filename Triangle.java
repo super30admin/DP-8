2f3b4c8aaceda597d8618b8ed26251f18fb783c7
Shivani31996 <shivani.chavan96@gmail.com>
2022-12-21 16:01:45 -0500

    Please review

--- Triangle.java
+++ Triangle.java
@@ -74,3 +74,56 @@ class Solution {
         return dp[0][0];
     }
 }
+
+
+/*
+ * Approach 4 - Using single array instead of using a matrix
+ */
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        
+        for(int j = 0; j < n; j++)
+        {
+            dp[j] = triangle.get(n-1).get(j);
+        }
+        
+        for(int i = n-2; i>=0; i--)
+        {
+            for(int j=0; j<triangle.get(i).size(); j++)
+            {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j],dp[j+1]);
+            }
+        }
+        return dp[0];
+    }
+}
+
+
+/*
+ * Approach 5 - Printing the path of chosen elements
+ */
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] path = new int[n][n];
+        
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j=0; j<triangle.get(i).size(); j++)
+            {
+                int min = triangle.get(i+1).get(j);
+                path[i][j] = j;
+                if(triangle.get(i+1).get(j) > triangle.get(i+1).get(j+1))
+                {
+                    min = triangle.get(i+1).get(j+1);
+                    path[i][j] = j+1;
+                }
+                triangle.get(i).set(j,triangle.get(i).get(j) + min);
+            }
+        }
+        System.out.println(Arrays.deepToString(path));
+        return triangle.get(0).get(0);
+    }
+}
