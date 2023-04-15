dad2b1e985897113b75fff7fdd979bc4b00f3b4b
Vamshi Sagar <gadde.vamshi2222@gmail.com>
2023-04-15 18:54:46 -0400

    Finished DP-8

---
+++ Problem1.java
@@ -0,0 +1,24 @@
+/*
+ArithmeticSlices
+approach: we can see if a, b, c can form a slice then a, b, c, d can also form a slice if diff is same
+time & space: O(n)
+ */
+public class Problem1 {
+    public int numberOfArithmeticSlices(int[] nums) {
+        if (nums==null||nums.length<3) return 0;
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = 0;
+        dp[1] = 0;
+        int rsum = 0;
+        for (int i=2;i<n;i++) {
+            int diff = nums[i-1]-nums[i-2];
+            if (nums[i]-nums[i-1]==diff) {
+                dp[i] = dp[i-1]+1;
+            }
+            else dp[i] = 0;
+            rsum+=dp[i];
+        }
+        return rsum;
+    }
+}
