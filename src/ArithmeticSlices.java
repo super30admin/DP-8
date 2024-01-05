e1735012e1260167e9341ad2b2029f2b698a8e2f
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2024-01-05 15:14:58 -0700

    Create ArithmeticSlices.java

---
+++ src/ArithmeticSlices.java
@@ -0,0 +1,42 @@
+// Time Complexity:  O(n)
+// Space Complexity: O(n)
+
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        if(n < 3) return count;
+        int dp[] = new int[n];
+        for(int i=n-3; i>=0; i--) {
+            if(nums[i+2] - nums[i+1] == nums[i+1] - nums[i])
+                dp[i] = dp[i+1]+1;
+        }
+        for(int d : dp)
+            count += d;
+        return count;
+    }
+}
+
+
+
+// // ******************** Another Approach ********************
+// // Time Complexity:  O(n^2)
+// // Space Complexity: O(1)
+
+// class Solution {
+//     public int numberOfArithmeticSlices(int[] nums) {
+//         int n = nums.length;
+//         int count = 0;
+//         if(n < 3) return count;
+//         for(int i=0; i<n-2; i++) {
+//             int diff = nums[i+1] - nums[i];
+//             for(int j=i+1; j<n-1; j++) {
+//                 if(nums[j+1] - nums[j] == diff)
+//                     count++;
+//                 else 
+//                     break;
+//             }
+//         }
+//         return count;
+//     }
+// }
