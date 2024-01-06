3124876faa17ddab842318fa6b4556e00baaefa9
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2024-01-05 22:14:00 -0700

    Update Triangle.java

--- src/Triangle.java
+++ src/Triangle.java
@@ -57,3 +57,57 @@ class Solution {
 //         return min;
 //     }
 // }
+
+
+
+
+// // ******************** DP ********************
+
+// // Time Complexity:  O(n)
+// // Space Complexity: O(n)
+
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int n = triangle.size();
+//         int[][] dp = new int[n][n];
+//         for(int j=0; j<n; j++) {
+//             dp[n-1][j] = triangle.get(n-1).get(j);
+//         }
+//         for(int i=n-2; i>=0; i--) {
+//             for(int j=0; j<=i; j++) {
+//                 dp[i][j] += triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// // ******************** If we want to get path of values ********************
+
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int n = triangle.size();
+//         List<List<Integer>> path = new ArrayList<>();
+//         for(int i=0; i<n-1; i++) {
+//             List<Integer> l = new ArrayList<>();
+//             path.add(l);
+//         }
+//         for(int i=n-2; i>=0; i--) {
+//             for(int j=0; j<=i; j++) {
+//                 if(triangle.get(i+1).get(j) > triangle.get(i+1).get(j+1)) path.get(i).add(j+1);
+//                 else path.get(i).add(j);
+//                 triangle.get(i).set(j, triangle.get(i).get(j) + Math.min(triangle.get(i+1).get(j), triangle.get(i+1).get(j+1)));
+//             }
+//         }
+//         System.out.println(Arrays.deepToString(path.toArray()));
+//         return triangle.get(0).get(0);
+//     }
+// }
