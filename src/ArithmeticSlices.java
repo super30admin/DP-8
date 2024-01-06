0e75d471031c652b4a114bccb31ee3599d823da5
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2024-01-05 20:22:53 -0700

    Update ArithmeticSlices.java

--- src/ArithmeticSlices.java
+++ src/ArithmeticSlices.java
@@ -1,3 +1,5 @@
+// // ******************** DP 1D ********************
+
 // Time Complexity:  O(n)
 // Space Complexity: O(n)
 
@@ -6,20 +8,44 @@ class Solution {
         int n = nums.length;
         int count = 0;
         if(n < 3) return count;
-        int dp[] = new int[n];
+        int curr = 0;
         for(int i=n-3; i>=0; i--) {
-            if(nums[i+2] - nums[i+1] == nums[i+1] - nums[i])
-                dp[i] = dp[i+1]+1;
+            if(nums[i+2] - nums[i+1] == nums[i+1] - nums[i]) {
+                curr++;
+                count += curr;
+            }
+            else {
+                curr = 0;
+            }
         }
-        for(int d : dp)
-            count += d;
         return count;
     }
 }
 
+// // ******************** DP array ********************
+
+// // Time Complexity:  O(n)
+// // Space Complexity: O(n)
+
+// class Solution {
+//     public int numberOfArithmeticSlices(int[] nums) {
+//         int n = nums.length;
+//         int count = 0;
+//         if(n < 3) return count;
+//         int dp[] = new int[n];
+//         for(int i=n-3; i>=0; i--) {
+//             if(nums[i+2] - nums[i+1] == nums[i+1] - nums[i])
+//                 dp[i] = dp[i+1]+1;
+//         }
+//         for(int d : dp)
+//             count += d;
+//         return count;
+//     }
+// }
 
 
-// // ******************** Another Approach ********************
+
+// // ******************** Brute Force ********************
 // // Time Complexity:  O(n^2)
 // // Space Complexity: O(1)
 
